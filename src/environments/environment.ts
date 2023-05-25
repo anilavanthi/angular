8e5605e2002b2de0fe286e21586b81e868d043d1
root <root@ip-172-31-25-236.us-east-2.compute.internal>
2023-05-25 15:39:37 +0000

    my first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000',
+  apiUrl: 'http://18.220.181.177:8000',
   oauthkey: "qCB4MNBGShxQ39DWDvZGG36ftCNa63NLJga4cVHH9gtxN9ddeh",
 };
